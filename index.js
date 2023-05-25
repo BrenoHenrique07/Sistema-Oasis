9e489d92d93acfe14473f440e76ac036736dc353
BrenoHenrique07 <brenohenriquess123@gmail.com>
2023-05-25 17:18:21 -0300

    Deletando arquivos para mudanca de estrutura

--- index.js
+++ index.js
@@ -8,20 +8,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-const pacientes = require('./src/paciente/routes');
-app.use('/pacientes', pacientes);
 
-const frequencias = require('./src/frequencia/routes');
-app.use('/frequencias', frequencias);
-
-const responsaveis = require('./src/responsavel/routes');
-app.use('/responsaveis', responsaveis);
-
-const doencas = require('./src/doencas/routes');
-app.use('/doencas', doencas);
-
-const db = require('./src/db/connect');
-db.syncDataBase();
 
 app.listen(3000, ()=>{
     console.log('Server is running');
